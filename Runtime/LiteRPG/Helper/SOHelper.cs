88b6ccbd187fc8c3b57c8bc91ad68cc2d2950634
Roman Maksymenko <maksimenko.roma@gmail.com>
2025-12-13 03:29:43 +0100

    Added SOHelper (for loading prefab from resources)

---
+++ Runtime/LiteRPG/Helper/SOHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace LiteRPG.Helper
+{
+    public class SOHelper
+    {
+        public static T LoadAndInstantiateSOFromResources<T>(string path) where T : ScriptableObject
+        {
+            T scriptableObject = Resources.Load(path, typeof(T)) as T;
+            if(scriptableObject == null)
+            {
+                Debug.LogError("Could not load scriptable object from path: " + path);
+                return null;
+            }
+
+            T instance = Object.Instantiate(scriptableObject);
+            instance.name = scriptableObject.name;
+            return instance;
+        }
+    }
+}
